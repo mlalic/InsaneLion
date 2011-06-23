2ec8abcce294b78fceb15faf1d9f9522d8cbec40
Marko Lalic <marko.lalic@gmail.com>
2011-06-23 21:51:16 +0200

    Malo napredniji Hello, World.

--- InsaneLion/video_stuff/views.py
+++ InsaneLion/video_stuff/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 # Create your views here.
 def default_view(request):
+    origHost = request.get_host()
+    return HttpResponse(origHost)
     return HttpResponse('Hello World...')
